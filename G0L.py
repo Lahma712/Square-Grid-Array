0b9c8ce6c24928a58d6433bb2299734c8a506675
Lahr Max <52128659+Lahma712@users.noreply.github.com>
2020-04-14 23:10:43 +0200

    Update G0L.py

--- G0L.py
+++ G0L.py
@@ -2,26 +2,27 @@ from PIL import Image, ImageDraw
 Im = Image.new("RGB", (1080, 2220))
 draw = ImageDraw.Draw(Im)
 
-def raster(Axis, a, b, size, WHp, CWH, P):
-
+def raster(Axis, a, b, size, HVp, CVH, P):
     for i in range(size):
         Axis[a] += [i]
-    A = WHp % CWH
+    A = HVp % CVH
     print(A)
     C = -1
     for _ in range(G):
         C = C + 1
         Axis[b] += [C]
+        
     for _ in range(A):
         C += P + 2
         Axis[b] += [C]
-
         for _ in range(G-1):
             C = C + 1
             Axis[b] += [C]
+
     if A > 0:
         C += P+1
-    for _ in range(CWH - A):
+
+    for _ in range(CVH - A):
         Axis[b] += [C]
         for _ in range(G-1):
             C = C + 1
@@ -33,23 +34,23 @@ def Draw(Axis, a, b):
         for x in Axis[a]:
             draw.point([x,y], (166,166,166))
 
-CW = int(input("number of cells in width: "))
-G = int(input("grid width: "))
-Wp = Im.size[0] - (CW +1)*G
-print("Wp: "+str(Wp))
-Pw = Wp//CW
-print("Pw: "+str(Pw))
-CH = Im.size[1]//Pw
-print("CH: "+str(CH))
-Hp = Im.size[1] - (CH +1)*G
-print("Hp: "+str(Hp))
-Ph = Hp // CH
-print("Ph: "+str(Ph))
+CH = int(input("number of horizonal cells: "))
+G = int(input("grid width: ")) #thickness of grid
+Hp = Im.size[0] - (CH +1)*G #available pixels for cells (grid pixels are subtracted)
+print("Wp: "+str(Hp))
+PH = Hp//CH #Pixel width per cell (horizontal pixels)
+print("Pw: "+str(PH))
+CV = Im.size[1]//PH # number of vertical cells
+print("CH: "+str(CV))
+Vp = Im.size[1] - (CV +1)*G #available pixels per cell (grid pixels are subtracted)
+print("Hp: "+str(Vp))
+PV = Vp // CV #Pixel height per cell (vertical pixels)
+print("Ph: "+str(PV))
 HGrid = [[], []]
 VGrid = [[], []]
 
-raster(HGrid, 0, 1, Im.size[0], Hp, CH, Ph)
-raster(VGrid, 1, 0, Im.size[1], Wp, CW, Pw)
+raster(VGrid, 1, 0, Im.size[1], Hp, CH, PH)
+raster(HGrid, 0, 1, Im.size[0], Vp, CV, PV)
 
 Draw(HGrid, 0, 1)
 Draw(VGrid, 0, 1)
