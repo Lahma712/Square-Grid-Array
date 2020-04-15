da3dcde8f86ff9e0727d1c8496249a8876108aae
Lahr Max <52128659+Lahma712@users.noreply.github.com>
2020-04-15 08:41:52 +0200

    Update G0L.py

--- G0L.py
+++ G0L.py
@@ -1,60 +1,72 @@
 from PIL import Image, ImageDraw
-Im = Image.new("RGB", (1080, 2220))
+width = int(input("screen width: "))
+height = int(input("screen height: "))
+Im = Image.new("RGB", (width, height))
 draw = ImageDraw.Draw(Im)
 
-def raster(Axis, a, b, size, HVp, CVH, P):
+def raster(Axis, a, b, size, CellPixelPool, CellCount, CellWidth, gridWidth, ExcessPixels):
     for i in range(size):
         Axis[a] += [i]
-    A = HVp % CVH
-    print(A)
-    C = -1
-    for _ in range(G):
+    C = -1 #counter for grid pixel position
+    for _ in range(gridWidth):
         C = C + 1
         Axis[b] += [C]
         
-    for _ in range(A):
-        C += P + 2
+    for _ in range(ExcessPixels):
+        C += CellWidth + 2
         Axis[b] += [C]
-        for _ in range(G-1):
+        for _ in range(gridWidth-1):
             C = C + 1
             Axis[b] += [C]
 
     
-    C += P+1
+    C += CellWidth+1
 
-    for _ in range(CVH -A):
+    for _ in range(CellCount - ExcessPixels):
         Axis[b] += [C]
-        for _ in range(G-1):
+        for _ in range(gridWidth-1):
             C = C + 1
             Axis[b] += [C]
-        C += P+ 1
+        C += CellWidth + 1
 
 def Draw(Axis, a, b):
     for y in Axis[b]:
         for x in Axis[a]:
             draw.point([x,y], (166,166,166))
 
-CH = int(input("number of horizonal cells: "))
-G = int(input("grid width: ")) #thickness of grid
-Hp = Im.size[0] - (CH +1)*G #available pixels for cells (grid pixels are subtracted)
-print("Wp: "+str(Hp))
-PH = Hp//CH #Pixel width per cell (horizontal pixels)
-print("Pw: "+str(PH))
-CV = Im.size[1]//PH # number of vertical cells
-print("CH: "+str(CV))
-Vp = Im.size[1] - (CV +1)*G #available pixels per cell (grid pixels are subtracted)
-print("Hp: "+str(Vp))
-PV = Vp // CV #Pixel height per cell (vertical pixels)
-print("Ph: "+str(PV))
+hCellCount = int(input("number of horizonal cells: "))
+gridWidth = int(input("grid width: ")) #thickness of grid
+
+hCellPixelPool = Im.size[0] - (hCellCount +1)*gridWidth #available pixels for cells (grid pixels are subtracted)
+print("hCellPixelPool: "+str(hCellPixelPool))
+
+hCellWidth = hCellPixelPool//hCellCount #Pixel width per cell (horizontal pixels)
+print("hCellWidth: "+str(hCellWidth))
+
+hExcessPixels = hCellPixelPool % hCellCount
+print("hExcessPixels: " + str(hExcessPixels))
+
+vCellCount = Im.size[1]//hCellWidth # number of vertical cells
+print("vCellCount: "+str(vCellCount))
+
+vCellPixelPool = Im.size[1] - (vCellCount +1)*gridWidth #available pixels per cell (grid pixels are subtracted)
+print("vCellPixelPool: "+str(vCellPixelPool))
+
+vCellWidth = vCellPixelPool // vCellCount#Pixel height per cell (vertical pixels)
+print("vCellWidth: "+str(vCellWidth))
+
+vExcessPixels = vCellPixelPool % vCellCount
+print("vExcessPixels: "+ str(vExcessPixels))
+
 HGrid = [[], []]
 VGrid = [[], []]
 
-raster(VGrid, 1, 0, Im.size[1], Hp, CH, PH)
-raster(HGrid, 0, 1, Im.size[0], Vp, CV, PV)
+raster(VGrid, 1, 0, Im.size[1], hCellPixelPool, hCellCount, hCellWidth, gridWidth, hExcessPixels)
+raster(HGrid, 0, 1, Im.size[0], vCellPixelPool, vCellCount, vCellWidth, gridWidth, vExcessPixels )
 
 Draw(HGrid, 0, 1)
 Draw(VGrid, 0, 1)
 print(HGrid)
 print(VGrid)
 Im.show()
-Im.save(r"C:\Users\Max\Desktop\G0L.png")
+Im.save(r"G0L.png")
